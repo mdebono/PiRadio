c1ade816be16198a8f5f171217bdf48f9ed5004d
Michael Debono <michael.debono@gmail.com>
2017-04-23 15:08:58 +0200

    Disabled shutdown and loads channels from CSV.

--- radio.py
+++ radio.py
@@ -1,50 +1,52 @@
-import glob, random, sys, vlc
+import glob, random, sys, vlc, os, csv
 from Adafruit_CharLCD import *
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 import time, subprocess
 
-channels = [
-    'http://stream.antenne1.de/stream1/livestream.mp3',
-    'http://jamfm.hoerradar.de/jamfm-mp3-128',
-    'http://s1.voscast.com:8132/',
-    'http://173.192.137.34:8044',
-    'http://188.94.97.91:80/radio21.mp3',
-	'http://shoutcast.pondi.hr:8000/listen.pls'
-]
-
-channelNames = [
-    'Antenne 1',
-    'JAM FM',
-    '89.7 Bay',
-    'Antena Zagreb',
-    'Radio 21 Hanover',
-	'Radio Dalmacija'
-]
-
-channel = 0
+channelsFilename = 'channels.csv'
 
 BUTTON_PLAY = 14
 BUTTON_NEXT = 15
 VOLUME_UP = 18
 VOLUME_DOWN = 10
 
+print('Starting PiRadio...')
+
 GPIO.setup(BUTTON_PLAY, GPIO.IN)
 GPIO.setup(BUTTON_NEXT, GPIO.IN)
 GPIO.setup(VOLUME_UP, GPIO.IN)
 GPIO.setup(VOLUME_DOWN, GPIO.IN)
 
-player = vlc.MediaPlayer()
-medialist = vlc.MediaList(channels)
-
-mlplayer = vlc.MediaListPlayer()
-mlplayer.set_media_player(player)
-mlplayer.set_media_list(medialist)
-
 lcd = Adafruit_CharLCD()
 
 default_lcd_text = None
 is_default_lcd_text = False
+
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+channelsFilepath = '{}/{}'.format(path, channelsFilename)
+print('Reading channels from {}...'.format(channelsFilepath))
+urls = []
+channelNames = []
+print('Channels:')
+with open(channelsFilepath) as f:
+    reader = csv.reader(f)
+    # skip header row
+    next(reader, None)
+    for row in reader:
+        if len(row) >= 2:
+            channelName = row[0]
+            url = row[1]
+            print('  {}'.format(channelName))
+            channelNames.append(channelName)
+            urls.append(url)
+
+player = vlc.MediaPlayer()
+medialist = vlc.MediaList(urls)
+
+mlplayer = vlc.MediaListPlayer()
+mlplayer.set_media_player(player)
+mlplayer.set_media_list(medialist)
 
 def message(m, is_permanent=True, visible_time=0):
     global default_lcd_text
@@ -67,16 +69,18 @@ def volume_change(delta):
     volume = player.audio_get_volume()
     volume = volume + delta
     player.audio_set_volume(volume)
-    message('Volume: {0}'.format(volume), False, 0.2)
+    message('Volume: {}'.format(volume), False, 0.2)
 
+channel = 0
 play(channel)
 
 while True:
-    if GPIO.input(VOLUME_UP) and GPIO.input(VOLUME_DOWN):
-        #TEST: subprocess.call(['shutdown', '-k', 'now', 'Radio initiated shutdown!'])
-        message('Shutting down...')
-        subprocess.call(['shutdown', '-h', 'now', 'Radio initiated shutdown!'])
-    elif GPIO.input(BUTTON_PLAY):
+    # TODO: Shutdown doesn't work anymore on Raspian Jessie, probably needs sudo
+    #if GPIO.input(VOLUME_UP) and GPIO.input(VOLUME_DOWN):
+    #    message('Shutting down...')
+    #    subprocess.call(['shutdown', '-h', 'now', 'Radio initiated shutdown!'])
+    #elif GPIO.input(BUTTON_PLAY):
+    if GPIO.input(BUTTON_PLAY):
         if mlplayer.is_playing():
             mlplayer.stop()
             message('Stopped :(')
